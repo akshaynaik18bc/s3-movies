1e0f40fcb7d77075ceb80079541452101e3403f8
akshay-at-892266108666 <akshay-at-892266108666>
2023-08-30 21:32:56 +0530

    initial commit 2

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -1,3 +1,42 @@
+// // src/components/MovieList.js
+// import React, { useState, useEffect } from 'react';
+// import { s3 } from '../awsConfig';
+
+// const MovieList = () => {
+//   const [movies, setMovies] = useState([]);
+
+//   useEffect(() => {
+//     // Fetch movie list from S3 bucket
+//     async function fetchMovies() {
+//       try {
+//         const response = await s3.listObjectsV2({ Bucket: 'movie-bucket-latest' }).promise();
+//         const movieList = response.Contents.map(item => item.Key);
+//         setMovies(movieList);
+//       } catch (error) {
+//         console.error('Error fetching movies:', error);
+//       }
+//     }
+
+//     fetchMovies();
+//   }, []);
+// // adding a comment
+//   return (
+//     <div>
+//       <h2>Movie List</h2>
+//       <ul>
+//         {movies.map((movie, index) => (
+//           <li key={index}>
+//             <a href={`/player/${encodeURIComponent(movie)}`}>{movie}</a>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default MovieList;
+
+import './MovieList.css'; // Import the CSS file
 // src/components/MovieList.js
 import React, { useState, useEffect } from 'react';
 import { s3 } from '../awsConfig';
@@ -19,17 +58,25 @@ const MovieList = () => {
 
     fetchMovies();
   }, []);
-// adding a comment
+
   return (
     <div>
       <h2>Movie List</h2>
-      <ul>
+      <div className="grid-container">
         {movies.map((movie, index) => (
-          <li key={index}>
-            <a href={`/player/${encodeURIComponent(movie)}`}>{movie}</a>
-          </li>
+          <div  key={index}>
+          <a href={`/player/${encodeURIComponent(movie)}`}>
+          <div className="grid-item" key={index}>
+            <img
+              src="https://viveklive.in/images/video-thumbnail.png" // Use your actual thumbnail URL here
+              alt={`Thumbnail for ${movie}`}
+            />
+            <p>{movie}</p>
+          </div>
+          </a>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
--- src/Components/VideoPlayer.js
+++ src/Components/VideoPlayer.js
@@ -2,15 +2,16 @@
 import React from 'react';
 import ReactPlayer from 'react-player';
 import { useParams } from 'react-router-dom';
+import "./MovieList.css";
 
 const VideoPlayer = ({ match }) => {
   const { movieKey } = useParams();
   //const movieKey = decodeURIComponent(match.params.movieKey);
 
   return (
-    <div>
-      <h2>Video Player</h2>
-      <ReactPlayer url={`https://movie-bucket-latest.s3.amazonaws.com/${movieKey}`} controls />
+    <div className='video-div'>
+      <h2>{movieKey}</h2>
+      <ReactPlayer style={{ width: '100%' }} url={`https://movie-bucket-latest.s3.amazonaws.com/${movieKey}`} controls />
     </div>
   );
 };
